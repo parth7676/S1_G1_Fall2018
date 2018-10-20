76886557b6e0ca15715d27dcb56476706d2bc5e6
Hardik Patel <hardikpatel.hardik36@gmail.com>
2018-10-19 20:15:12 -0400

    fix problem with saving enum in db

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from enum import Enum
 
+
 # Create your models here.
 class Property_Category_Choices(Enum):
     SingleHouse = 'singleHouse'
@@ -14,6 +15,7 @@ class Property_Category_Choices(Enum):
     Building = 'building'
     Other = 'other'
 
+
 class Property_Sector_Choices(Enum):
     Private = 'private'
     Residential = 'residential'
@@ -22,6 +24,7 @@ class Property_Sector_Choices(Enum):
     Rural = 'rural'
     Other = 'other'
 
+
 class Property_Facing_Choices(Enum):
     North = 'north'
     South = 'south'
@@ -29,39 +32,63 @@ class Property_Facing_Choices(Enum):
     West = 'west'
     Other = 'other'
 
+
 class Country(models.Model):
     def __str__(self):
         return self.countryName
 
     countryName = models.CharField(max_length=50)
 
+
 class Province(models.Model):
     def __str__(self):
         return self.provinceName
+
     countryID = models.ForeignKey(Country, on_delete=models.CASCADE)
     provinceName = models.CharField(max_length=50)
+
 
 class City(models.Model):
     def __str__(self):
         return self.cityName
+
     cityName = models.CharField(max_length=50)
     countryID = models.ForeignKey(Country, on_delete=models.CASCADE)
     provinceID = models.ForeignKey(Province, on_delete=models.CASCADE)
 
+
 class Property_Category(models.Model):
-    propertyCategory = models.CharField(max_length=20, choices=[(tag, tag.value) for tag in Property_Category_Choices], default=Property_Category_Choices.SingleHouse)
+    def __str__(self):
+        return self.propertyCategory
+
+    propertyCategory = models.CharField(max_length=100, choices=[(tag.value, tag.name) for tag in Property_Category_Choices],
+                                        default=Property_Category_Choices.SingleHouse, unique=True)
+
 
 class Property_Sector(models.Model):
-    propertySector = models.CharField(max_length=20, choices=[(tag, tag.value) for tag in Property_Sector_Choices], default=Property_Sector_Choices.Commercial)
+    def __str__(self):
+        return self.propertySector
+
+    propertySector = models.CharField(max_length=100, choices=[(tag.value, tag.name) for tag in Property_Sector_Choices],
+                                      default=Property_Sector_Choices.Commercial,unique=True)
+
 
 class Property_Facing(models.Model):
-    propertyFacing = models.CharField(max_length=20, choices=[(tag, tag.value) for tag in Property_Facing_Choices], default=Property_Facing_Choices.North)
+    def __str__(self):
+        return self.propertyFacing
+
+    propertyFacing = models.CharField(max_length=100, choices=[(tag.value, tag.name) for tag in Property_Facing_Choices],
+                                      default=Property_Facing_Choices.North, unique=True)
+
 
 class Property(models.Model):
+    def __str__(self):
+        return self.propertyTitle
+
     propertyID = models.AutoField(primary_key=True)
     propertyTitle = models.CharField(max_length=50)
-    propertyCategory = models.ForeignKey(Property_Category, on_delete=models.SET_NULL, null= True)
-    propertySector = models.ForeignKey(Property_Sector, on_delete=models.SET_NULL, null= True)
+    propertyCategory = models.ForeignKey(Property_Category, on_delete=models.SET_NULL, null=True)
+    propertySector = models.ForeignKey(Property_Sector, on_delete=models.SET_NULL, null=True)
     propertyFacing = models.ForeignKey(Property_Facing, on_delete=models.SET_NULL, null=True)
     propertyCountry = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
     propertyProvince = models.ForeignKey(Province, on_delete=models.SET_NULL, null=True)
@@ -79,14 +106,12 @@ class Property(models.Model):
     propertyAskingPrice = models.FloatField()
     propertySellingPrice = models.FloatField()
 
+
 class Property_Images(models.Model):
+    def __str__(self):
+        return str(self.propertyID)+"  :  "+str(self.propertyImageId)
+
     propertyID = models.ForeignKey(Property, on_delete=models.CASCADE)
+    propertyImageId = models.AutoField(primary_key=True)
     propertyImage = models.ImageField(upload_to='images', max_length=255, null=True, blank=True)
-    propertyImageID = models.CharField(max_length=50)
     propertyImageDescription = models.CharField(max_length=255, null=True, blank=True)
-
-
-
-
-
-
