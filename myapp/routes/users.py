7bcc4be73f6e9d75a1c675108f1d1310ee373833
parth7676 <parth7676@gmail.com>
2018-11-05 15:56:07 -0500

    minor code changes.

--- myapp/routes/users.py
+++ myapp/routes/users.py
@@ -3,5 +3,5 @@ from myapp.partials_views import accounts
 
 routes = [
     path('users/', accounts.UsersView.as_view(), name='users'),
-    path('users/activate', accounts.activate_user)
+    path('users/activate/', accounts.activate_user)
 ]
