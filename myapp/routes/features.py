914800a26c7d294c72dcb81d1e25e26159fd54e5
Siddharth Thakor <sidht1295@gmail.com>
2018-11-12 16:13:37 -0500

    Added feature delete functionality

--- myapp/routes/features.py
+++ myapp/routes/features.py
@@ -5,5 +5,6 @@ routes = [
     path('features/', features.FeaturesListView.as_view(), name='features'),
     path('features/add/', features.create_feature_view, name='add-feature'),
     re_path(r'^features/edit/(?P<role_permission_id>[0-9]+)/$', features.update_feature_view, name='update-feature'),
+    re_path(r'^feature/delete/(?P<role_permission_id>[0-9]+)/$', features.feature_delete_view, name='delete-feature')
 ]
 
